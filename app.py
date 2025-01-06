867c5fae1cbd8e40a7a583591b362c96b891c0b5
many-to-one <xllxlex@gmail.com>
2025-01-06 20:42:09 +0100

    create: ConnectionUsersManager for users websocket connection

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 
+from schemas.users import UserBase
+from security.security import get_current_user
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
@@ -52,7 +54,7 @@ class ConnectionManager:
         await websocket.accept()
         self.active_connections[chat_id] = websocket
         # self.active_connections[user_id] = websocket
-        # print('************* WEBSOCCET IS CONNECTED ***************')
+        print('************* WEBSOCCET IS CONNECTED ***************')
 
     def disconnect(self, websocket: WebSocket):
         user_id = next((uid for uid, ws in self.active_connections.items() if ws == websocket), None)
@@ -96,70 +98,210 @@ async def websocket_endpoint(
     db: AsyncSession = Depends(get_db)
 ):
 
-    await manager.connect(websocket, client_id)
+    await manager.connect(websocket, sender_id)
 
     try:
         while True:
-            # Receive message from client
-            data = await websocket.receive_json()  # Expecting JSON with sender, receiver, and message
-            print('SENDED DATA', data)
 
-            if data["message"] == "chat_onopen":
-                await manager.broadcast(f'chat_onopen {data['sender_id']} is active')
+            try:
+                data = await websocket.receive_json()
+                print('SENDED DATA:', data)
 
-            # If use 'else' - will be send a message 'chat_onopen' or 'chat_onclose'
-            # But if use 'elif...' will be send only real user's message
-            elif data["message"] != "chat_onopen" and data["message"] != "chat_onclose":
-                sender_id = data["sender_id"]
-                receiver_id = data["receiver_id"]
-                message = data["message"]
+                # Broadcast "chat_onopen" message
+                if data["message"] == "chat_onopen":
+                    await manager.broadcast(f"chat_onopen {data['sender_id']} is active")
+                    is_active = await manager.is_user_online(data['receiver_id'])
+                    print(' ################ chat_onopen is_active ################ ', is_active)
 
-                # print(' ################ SENDED USER MESSAGE ################ ', message)
+                elif data["message"] == "receiver_active":
+                    print(' ################ receiver_active ################ ', data)
+                    # await manager.broadcast(f"is_active")
 
-                result = await db.execute(
-                    select(Chat)
-                    .join(chat_users)
-                    .filter(chat_users.c.user_id.in_([sender_id, receiver_id]))
-                    .group_by(Chat.id)
-                    .having(func.count(chat_users.c.user_id) == 2)  # Ensure both users exist in the same chat
-                    .options(selectinload(Chat.messages))
-                )
-                obj = result.scalars().first()
+                # If use 'else' - will be send a message 'chat_onopen' or 'chat_onclose'
+                # But if use 'elif...' will be send only real user's message
+                elif data["message"] != "chat_onopen" and data["message"] != "chat_onclose":
+                    sender_id = data["sender_id"]
+                    receiver_id = data["receiver_id"]
+                    is_active = await manager.is_user_online(receiver_id)
+                    print(' ################ send is_active ################ ', is_active)
+                    message = data["message"]
 
-                if obj is None:
-                    # Save message in the database
-                    chat_form = {
-                        "sender_id": sender_id,
-                        "receiver_id": receiver_id,
-                        "messages": [Message(message=message, read=True)]
-                    }
-                    __orm = OrmService(db)
-                    new_chat = await __orm.create(model=Chat, form=chat_form)
-                    await db.execute(
-                        chat_users.insert().values([
-                            {"chat_id": new_chat.id, "user_id": sender_id},
-                            {"chat_id": new_chat.id, "user_id": receiver_id}
-                        ])
+                    print(' ################ SENDED USER MESSAGE ################ ', message)
+
+                    result = await db.execute(
+                        select(Chat)
+                        .join(chat_users)
+                        .filter(chat_users.c.user_id.in_([sender_id, receiver_id]))
+                        .group_by(Chat.id)
+                        .having(func.count(chat_users.c.user_id) == 2)  # Ensure both users exist in the same chat
+                        .options(selectinload(Chat.messages))
                     )
-                    await db.commit()
+                    obj = result.scalars().first()
 
-                    obj = new_chat
-
-                else:
-                    # Handle offline messaging
-                    new_message = Message(
-                        message=message, 
-                        chat_id=obj.id,
-                        user_id=sender_id,
-                        read=False,
+                    if obj is None:
+                        # Save message in the database
+                        chat_form = {
+                            "sender_id": sender_id,
+                            "receiver_id": receiver_id,
+                            "messages": [Message(message=message, read=is_active)]
+                        }
+                        __orm = OrmService(db)
+                        new_chat = await __orm.create(model=Chat, form=chat_form)
+                        await db.execute(
+                            chat_users.insert().values([
+                                {"chat_id": new_chat.id, "user_id": sender_id},
+                                {"chat_id": new_chat.id, "user_id": receiver_id}
+                            ])
                         )
-                    db.add(new_message)
-                    await db.commit()
-                    await db.refresh(new_message)
+                        await db.commit()
 
-                # Broadcast message to everyone (optional)
-                await manager.broadcast(f"Client #{sender_id} to #{receiver_id}: {message}")
+                        obj = new_chat
+
+                    else:
+                        # Handle messaging
+                        new_message = Message(
+                            message=message, 
+                            chat_id=obj.id,
+                            user_id=sender_id,
+                            read=is_active,
+                            )
+                        db.add(new_message)
+                        await db.commit()
+                        await db.refresh(new_message)
+
+                    await manager.broadcast(f"Client #{sender_id} to #{receiver_id}: {message}, receiver_active: {is_active}")
+
+            except WebSocketDisconnect:
+                print(f"WebSocket client_id {client_id} disconnected.")
+                manager.disconnect(websocket)
+                break
+            except Exception as e:
+                print(f"Error in WebSocket: {e}")
+                await websocket.close()
+                break
+
 
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client #{sender_id} left the chat")
+
+
+
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import WebSocket, WebSocketDisconnect, Depends
+from sqlalchemy.future import select
+import json
+
+
+class ConnectionUsersManager:
+    def __init__(self):
+        # Ensure this is a dictionary, not a list
+        self.active_connections: dict[int, WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, chat_id: int):
+        await websocket.accept()
+        self.active_connections[chat_id] = websocket
+        # self.active_connections[user_id] = websocket
+        print('************* WEBSOCCET IS CONNECTED ***************')
+
+    def disconnect(self, websocket: WebSocket):
+        user_id = next((uid for uid, ws in self.active_connections.items() if ws == websocket), None)
+        if user_id:
+            del self.active_connections[user_id]
+
+
+    async def is_user_online(self, user_id: str) -> bool:
+        return user_id in self.active_connections
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        if message.startswith == 'chat_onopen':
+            await websocket.send_text(message)
+        if message.startswith == 'chat_onclose':
+            await websocket.send_text(message)
+        else:
+            await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections.values():
+            await connection.send_text(message)
+
+    def get_connection(self, user_id: int) -> WebSocket | None:
+        return self.active_connections.get(user_id)
+
+users_manager = ConnectionUsersManager()
+
+# All chats of user
+@app.websocket("/ws/")
+async def websocket_endpoint(
+    websocket: WebSocket, 
+    token: str, 
+    db: AsyncSession = Depends(get_db),
+):
+
+    # Validate token and authenticate user
+    if token:
+        current_user = await get_current_user(token, db)
+
+        if current_user:
+            print(' *********** WEBSOCKET CURRENT USER ID *********** ', current_user.id)
+            await users_manager.connect(websocket, current_user.id)
+
+        else:
+            await websocket.close(code=1008)  # Close WebSocket if unauthorized
+            await users_manager.broadcast("401 Unauthorized")
+            return
+    else:
+        await websocket.close(code=1008)  # Close WebSocket if no token
+        await users_manager.broadcast("No token")
+        return
+
+    try:
+        while True:
+            # Wait for a message or event from the client
+            data = await websocket.receive_json()  # Expecting JSON with sender, receiver, and message
+            print(' ************** RECEIVED FOR INDEX DATA ************** ', data)
+            await users_manager.broadcast(f"{data}")
+
+    except WebSocketDisconnect:
+        users_manager.disconnect(websocket)
+        await users_manager.broadcast(json.dumps({
+            "message": f"{current_user.id} left chats area"
+        }))
+
+    except Exception as e:
+        print("WebSocket Error:", e)
+        await websocket.close(code=1011)
+
+
+
+# @app.websocket("/ws/")
+# async def websocket_endpoint(
+#     websocket: WebSocket, 
+#     token: str, 
+#     db: AsyncSession = Depends(get_db),
+# ):
+
+#     if token:
+#         print(' *********** WEBSOCKET TOKEN 2 *********** ', token)
+#         current_user = await get_current_user(token, db)
+
+#         if current_user:
+#             print(' *********** WEBSOCKET CURRENT USER ID *********** ', current_user.id)
+#             await manager.connect(websocket, current_user.id)
+#     else:
+#         manager.disconnect(websocket)
+#         await manager.broadcast(f"Unauthtorized")
+
+
+#     try:
+#         while True:
+#             result = await db.execute(
+#                 select(Message).
+#                 where(Message.user_id == current_user.id)
+#             )
+#             obj = result.scalars().first()
+#             print(' ********* USERS MESSAGES ********* ', obj)
+#     except WebSocketDisconnect:
+#         manager.disconnect(websocket)
+#         await manager.broadcast(f"{current_user.id} left chats area")
